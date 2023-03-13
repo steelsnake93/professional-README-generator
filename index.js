a5e6abeb72caf1c0c437500ce6bbf7c916c02825
steelsnake93 <nikolanikotasev@protonmail.com>
2023-03-13 19:52:16 +0100

    Added array of questions for user

--- index.js
+++ index.js
@@ -5,7 +5,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    },
+    {
+    type: "input",
+    name: "description",
+    message: "Please provide a description of your project:",
+    },
+    {
+    type: "input",
+    name: "installation",
+    message: "Please provide installation instructions for your project:",
+    },
+    {
+    type: "input",
+    name: "usage",
+    message: "Please provide usage information for your project:",
+    },
+    {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use for your project?",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+    {
+    type: "input",
+    name: "contributing",
+    message: "Please provide contribution guidelines for your project:",
+    },
+    {
+    type: "input",
+    name: "tests",
+    message: "Please provide test instructions for your project:",
+    },
+    {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+    },
+    {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+    },
 ];
 
 // function to write README file
